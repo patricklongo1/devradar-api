62d488d66eed045d4d34b2b1eb562a0278a1dbb3
Patrick Longo <ck.longo@bol.com.br>
2020-01-14 08:57:56 -0300

    routes / server

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const routes = new Router();
+// import Controller from './Controllers/Controller';
+
+// routes.post('/boxes', Controller.store);
+
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import routes from './routes';
+
+mongoose.connect('mongodb://localhost:27017/rocketbox', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Opa' });
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
